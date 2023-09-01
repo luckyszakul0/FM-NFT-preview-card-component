e5e72ab85184ffcf9ec1f274b109cc2eb8db18cc
Lukasz710 <lubaszewskilukasz0@gmail.com>
2023-09-01 11:21:05 +0200

    done the image portion but still some issues

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 //the blob is VERY HEAVILY inspired from Hyperplexed's codepen and YT video https://codepen.io/Hyperplexed/pen/KKBjvbG
 
 const blob = document.getElementById("blob");
+const enlarged = document.querySelector(".enlarged");
+const dimmer = document.querySelector(".dimmer");
 
 // Check if the device has touch support, if yes hide the blob on those devices to dodge spacing and scrolling problems
 if ('ontouchstart' in window || navigator.maxTouchPoints) {
@@ -18,3 +20,19 @@ if ('ontouchstart' in window || navigator.maxTouchPoints) {
         });
     }
 }
+
+document.querySelector(".nft").addEventListener("click", () => {
+    enlarged.style.display = "flex";
+    enlarged.style.animation = "slideIn 0.5s forwards";
+    dimmer.style.display = "block";
+    dimmer.style.animation = "dimWebsite 0.5s forwards"
+})
+
+document.querySelector("#exit").addEventListener("click", () => {
+    enlarged.style.animation = "slideOut 0.5s forwards";
+    dimmer.style.animation = "undimWebiste 0.5s forwards";
+    setTimeout(() => {
+        enlarged.style.display = "none";
+        dimmer.style.display = "none";
+    }, 500);
+})
