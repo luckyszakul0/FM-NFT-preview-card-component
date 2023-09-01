c73daa4816ab008e01c219f29a5b60a483dcaec4
Lukasz710 <lubaszewskilukasz0@gmail.com>
2023-09-01 12:46:25 +0200

    finished the challenge for now

--- js/main.js
+++ js/main.js
@@ -4,6 +4,29 @@ const blob = document.getElementById("blob");
 const enlarged = document.querySelector(".enlarged");
 const dimmer = document.querySelector(".dimmer");
 
+//I've used variables for animating to reverse the dimming and spare some lines of code
+const dimWebsite = [
+    {
+        opacity: 0
+    },
+    {
+        opacity: 0.75
+    }
+];
+
+const animateParams = {
+    duration: 500,
+    fill: 'forwards',
+    easing: 'ease-in-out'
+};
+
+const reversedAnimateParams = {
+    duration: 500,
+    fill: 'forwards',
+    easing: 'ease-in-out',
+    direction: 'reverse'
+}
+
 // Check if the device has touch support, if yes hide the blob on those devices to dodge spacing and scrolling problems
 if ('ontouchstart' in window || navigator.maxTouchPoints) {
     blob.style.display = 'none';
@@ -22,17 +45,45 @@ if ('ontouchstart' in window || navigator.maxTouchPoints) {
 }
 
 document.querySelector(".nft").addEventListener("click", () => {
+
+    //animation of sliding in the enlarged nft
     enlarged.style.display = "flex";
-    enlarged.style.animation = "slideIn 0.5s forwards";
+    enlarged.animate([
+        {
+            transform: 'translateY(-150px)',
+            opacity: 0
+        },
+        {
+            transform: 'translateY(0)',
+            opacity: 1
+        }
+    ], animateParams)
+
+    //animation of dimming the website down
     dimmer.style.display = "block";
-    dimmer.style.animation = "dimWebsite 0.5s forwards"
+    dimmer.animate(dimWebsite, animateParams)
+
 })
 
 document.querySelector("#exit").addEventListener("click", () => {
-    enlarged.style.animation = "slideOut 0.5s forwards";
-    dimmer.style.animation = "undimWebiste 0.5s forwards";
+    //animation of sliding the enlarged nft out
+    enlarged.animate([
+        {
+            transform: 'translateY(0)',
+            opacity: 1
+        },
+        {
+            transform: 'translateY(150px)',
+            opacity: 0
+        }
+    ], animateParams)
+
+    //animation of undimming the website with an addition property of direction: reverse as to save some lines of code for writing a new animation for it
+    dimmer.animate(dimWebsite, reversedAnimateParams);
+
+    //setting the display property of pop-up elements to none as to be able to click the permament content on the page
     setTimeout(() => {
         enlarged.style.display = "none";
         dimmer.style.display = "none";
-    }, 500);
+    }, 500)
 })
