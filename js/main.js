32ad1d17d3033118f02aa6f5961a5b4afe46ec6f
Lukasz710 <lubaszewskilukasz0@gmail.com>
2023-09-01 09:34:19 +0200

    finished the blob

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
+//the blob is VERY HEAVILY inspired from Hyperplexed's codepen and YT video https://codepen.io/Hyperplexed/pen/KKBjvbG
+
 const blob = document.getElementById("blob");
 
-window.onpointermove = event => { 
-    const { clientX, clientY } = event;
+// Check if the device has touch support, if yes hide the blob on those devices to dodge spacing and scrolling problems
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    blob.style.display = 'none';
+} else {  //make the blob follow the cursor with a bit of a delay
+    window.onpointermove = event => { 
+        const { clientX, clientY } = event;
     
-    blob.animate({
-        left: `${clientX}px`,
-        top: `${clientY}px`
-    }, {
-        duration: 3000,
-        fill: "forwards"
-    });
+        blob.animate({
+            left: `${clientX}px`,
+            top: `${clientY}px`
+        }, {
+            duration: 3000,
+            fill: "forwards"
+        });
+    }
 }
